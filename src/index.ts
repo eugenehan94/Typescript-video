7e6437685da7efde29f13dda73c9e537c5cbe1d6
eugenehan94 <eugene.han94@gmail.com>
2022-06-26 14:25:42 -0400

    first commit of typescript video project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+const btn = document.querySelector<HTMLButtonElement>(".switch-btn");
+const video = document.querySelector<HTMLVideoElement>(".video-container");
+
+btn?.addEventListener("click", () => {
+    if(!btn.classList.contains("slide")){
+        btn.classList.add("slide");
+        video?.pause();
+    }else{
+        btn.classList.remove("slide");
+        video?.play();
+    }
+});
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", () => {
+    preloader?.classList.add("hide-preloader")
+})
+
 export {}
